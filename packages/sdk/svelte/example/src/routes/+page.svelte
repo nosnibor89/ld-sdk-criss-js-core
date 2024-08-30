cc57ec9516849fa89db0f6b0d224211af90e330a
Cris Ocaña <cris.ocana@moduscreate.com>
2024-08-30 00:23:48 -0600

    Svelte SDK example app

---
+++ packages/sdk/svelte/example/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { LDProvider } from '@launchdarkly/svelte-client-sdk';
+  import { LDFlag } from '@launchdarkly/svelte-client-sdk';
+  const context = {
+    kind: 'user',
+    key: 'example-context-key',
+    name: 'Sandy'
+  };
+</script>
+<LDProvider clientID="" {context}>
+  <LDFlag flag={'example-flag'}>
+    <div slot="true">
+      <p>this will render if the feature flag is on</p>
+    </div>
+    <div slot="false">
+      <p>this will render if the feature flag is off</p>
+    </div>
+  </LDFlag>
+</LDProvider>
